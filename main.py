442331ba79721dd381c51cb00276c9fafc4eb2c6
E-W-R <evan.rossiter@uwaterloo.ca>
2022-10-11 17:31:17 -0400

    Submission #1

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import sys
 import chess
+import random
 
-colour = sys.argv[1]
+#colour = sys.argv[1]
 
-board = chess.Board()
+class AntiBoard(chess.Board):
+    @property
+    def legal_moves(self):
+        return MyLegalMoveGenerator(self)
+
+class MyLegalMoveGenerator(chess.LegalMoveGenerator):
+    def __iter__(self):
+        return self.board.generate_legal_captures() if any(self.board.generate_legal_captures()) \
+         else self.board.generate_legal_moves()
+
+board = AntiBoard()
+
+def RandomMove():
+    moves = list(board.legal_moves)
+    move = moves[random.randint(0,len(moves)-1)]
+    board.push(moves[random.randint(0,len(moves)-1)])
+    print(move)
+
+#board.legal_moves
+
+while True:
+    input()
+    RandomMove()
+    print(board)
